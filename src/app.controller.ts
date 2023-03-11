839e1fcf49df1d1585d3c669c35eb38fa8e2a0bf
juracucumber1 <mr.gordien@mail.ru>
2023-03-11 04:37:09 +0300

    update
    -Контроллеры, провайдеры (сервисы), @Module, @Controller, @Injectable.
    -Внедрение зависимостей.
    -

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
+@Controller('/api')
+export class AppController{
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  constructor(private appService: AppService) {}
+
+
+    @Get('/users')
+    getUsers(){
+      return this.appService.getUsers()
+}
 }
